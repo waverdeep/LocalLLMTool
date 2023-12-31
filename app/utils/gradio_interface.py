26663dff5ba40e4b6558f9f85cefd81695ae65a3
waverDeep <waverdeep@gmail.com>
2023-12-31 14:26:15 +0000

    FIX: update gui code

--- app/utils/gradio_interface.py
+++ app/utils/gradio_interface.py
@@ -11,6 +11,11 @@ def echo(message, history, system_prompt, tokens):
 
 demo = gr.ChatInterface(
     echo, 
+    chatbot=gr.Chatbot(height=300),
+    textbox=gr.Textbox(placeholder="Ask me a yes or no question", container=False, scale=7),
+    title="Yes Man",
+    description="Ask Yes Man any question",
+    theme="soft",
     stop_btn=None,
     additional_inputs=[
         gr.Textbox("You are helpful AI.", label="System Prompt"), 
