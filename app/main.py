52a307b9408cf771852bba4b64934967ce3b656d
waverDeep <waverdeep@gmail.com>
2023-12-31 13:51:07 +0000

    FIX: update parameter

--- app/main.py
+++ app/main.py
@@ -4,5 +4,5 @@ import utils.gradio_interface as gradio_interface
 if __name__ == '__main__':
     gradio_interface.demo.launch(
         server_name="0.0.0.0",
-        server_port="8000"
+        server_port=8000
     )
