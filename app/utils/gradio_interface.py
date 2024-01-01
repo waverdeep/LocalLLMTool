c5cf9ab2cf378cf4069e4feaefa9e220177779a2
waverDeep <waverdeep@gmail.com>
2024-01-01 03:18:46 +0000

    FIX: update gui code

--- app/utils/gradio_interface.py
+++ app/utils/gradio_interface.py
@@ -2,6 +2,10 @@ import gradio as gr
 import time
 
 
+def allocate_model():
+    pass
+
+
 def echo(message, history, system_prompt, tokens):
     response = f"System prompt: {system_prompt}\n Message: {message}."
     for i in range(len(response)):
@@ -13,20 +17,23 @@ chat = gr.ChatInterface(
                 echo, 
                 chatbot=gr.Chatbot(height="50vh"),
                 textbox=gr.Textbox(
-                    placeholder="Ask me a question", 
-                    container=False, scale=7),
+                    placeholder="ask me a question", 
+                    container=False, 
+                    scale=7
+                ),
                 stop_btn=None,
                 additional_inputs=[
                     gr.Textbox(
                         "You are helpful AI.", 
-                        label="System Prompt"), 
+                        label="system prompt"), 
                     gr.Slider(
                         minimum=0, 
                         maximum=1, 
                         step=0.1,
                         value=0.5,
                         label="temperature")
-                ]
+                ],
+                label="multi-turn"
             ).queue()
 
 with gr.Blocks(theme="soft") as demo:
@@ -38,17 +45,24 @@ with gr.Blocks(theme="soft") as demo:
     with gr.Row():
         with gr.Column():
             with gr.Group():
-                gr.Dropdown(
+                model_dropdown = gr.Dropdown(
                     choices=[
                         "Mistral-7B-OpenOrca-GGUF",
                         "OpenAI-GPT-3.5-Turbo"
                     ],
                     value="Mistral-7B-OpenOrca-GGUF",
+                    interactive=True,
+                    label="selet model"
                 )
-                gr.Button(
+                allocate_button = gr.Button(
                     value="Allocate"
                 )
-            
+                allocate_button.click(
+                    fn=allocate_model,
+                    inputs=model_dropdown,
+                    outputs=None,
+                    api_name="allocate_model"
+                )
         with gr.Column():
             chat.render()
     
