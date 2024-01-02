4e1f8a7769fc948c7786198faa8497811a3a8d45
waverDeep <waverdeep@gmail.com>
2024-01-02 14:51:24 +0000

    FIX: update variable

--- app/utils/util.py
+++ app/utils/util.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 
 
 def register_openai_api_key(api_key):
-    if secret_key:
-        return OpenAI(api_key=secret_key)
+    if api_key:
+        return OpenAI(api_key=api_key)
     else:
         return None
