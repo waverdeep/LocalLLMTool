8436ede71f0e30ab70419c1eca8ec6063096af7f
waverDeep <waverdeep@gmail.com>
2024-01-03 15:48:22 +0000

    FIX: 데이터프레임 추가

--- app/utils/gradio_interface.py
+++ app/utils/gradio_interface.py
@@ -15,52 +15,17 @@ config = {
     "MY_ACCESS_KEY": os.environ.get('MY_ACCESS_KEY', None),
 }
 
-custom_session = []
-
 chat_client = util.register_openai_api_key(config['OPENAI_API_KEY'])
 
 
-def predict(message, history, model_name, system_prompt, temperature, access_key):
-    global chat_client
-    global allowed_models
-    global config
-
-    if "gpt" in model_name and chat_client is None:
-        yield "Please register the openai api key."
-        return
-
-    if model_name not in allowed_models:
-        yield "Please select the model again."
-        return
-
-    if access_key != config['MY_ACCESS_KEY']:
-        yield "Please check the my access key."
-        return
-
-    history_openai_format = []
-    if system_prompt != "":
-        history_openai_format.append({"role": "system", "content": system_prompt })
-    for human, assistant in history:
-        history_openai_format.append({"role": "user", "content": human })
-        history_openai_format.append({"role": "assistant", "content":assistant})
-    history_openai_format.append({"role": "user", "content": message})
-
-    stream = chat_client.chat.completions.create(
-        model=model_name,
-        messages= history_openai_format,
-        temperature=temperature,
-        stream=True
+def save_chat_history(history):
+    chat_history.input(
+        inputs=[39203.20, "hello?"]
     )
-
-    partial_message = ""
-    for chunk in stream:
-        if chunk.choices[0].delta.content is not None:
-            partial_message = partial_message + chunk.choices[0].delta.content
-            yield partial_message
 
 
 chat = gr.ChatInterface(
-    predict,
+    util.predict,
     chatbot=gr.Chatbot(height="60vh"),
     textbox=gr.Textbox(
         placeholder="무엇이든 물어보세요!", 
@@ -92,16 +57,26 @@ chat = gr.ChatInterface(
     ],
 ).queue()
 
+
 with gr.Blocks(theme="soft", title="MLT",) as demo:
     with gr.Row():
-        with gr.Column():
+        with gr.Column(scale=7):
             chat.render()
-    with gr.Row():
-        with gr.Column():
+        with gr.Column(scale=3):
             with gr.Blocks():
                 gr.Label(
                     value="Additional Tools"
                 )
                 save_button = gr.Button(
-                    value="Save",
+                    value="Save chat history",
+                )
+                save_button.click(
+                    save_chat_history,
+                    inputs=[chat.history],
+                    outputs=None
+                )
+                chat_history = gr.DataFrame(
+                    headers=["session_id", "title"],
+                    datatype=["float", "str"],
+                    value=None,
                 )
