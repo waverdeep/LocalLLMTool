7c6ef1189f47a6e6d478e97ef7c96aaf88945ffd
waverDeep <waverdeep@gmail.com>
2024-01-14 08:10:05 +0000

    FIX: port

--- app/main.py
+++ app/main.py
@@ -5,5 +5,5 @@ import utils.user_interface as user_interface
 if __name__ == '__main__':
     user_interface.demo.launch(
         server_name="0.0.0.0",
-        server_port=6007
+        server_port=8000
     )
