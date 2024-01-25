d99df743a54996a6a33e08d4094bb9bf1f8285fc
waverdeep <dandyleader@naver.com>
2024-01-26 00:46:45 +0900

    FIX: 파일구조 변경

---
+++ gradio_app/custom_chat/callbacks.py
@@ -0,0 +1,45 @@
+import time
+
+from utils import util
+from utils import configs
+
+
+chat_client = util.register_openai_api_key(configs.config['OPENAI_API_KEY'])
+
+
+def user(chat_id, message, history):
+    if not chat_id:
+        chat_id = time.time()
+    return chat_id, "", history + [[message, None]]
+
+
+def chat_completion(history, model_name, connect_key, temperature, system_prompt):
+    global chat_client
+
+    history[-1][1] = ""
+
+    if "gpt" in model_name and chat_client is None:
+        history[-1][1] += "Please register the openai api key."
+        yield history
+        return
+    if model_name not in configs.allowed_models:
+        history[-1][1] += "Please select the model again."
+        yield history
+        return
+    if connect_key != configs.config['MY_ACCESS_KEY']:
+        history[-1][1] += "Please check the connect key."
+        yield history
+        return
+    
+    stream = chat_client.chat.completions.create(
+        model=model_name,
+        messages= util.get_conversation(system_prompt, history, history[-1][0]),
+        temperature=temperature,
+        stream=True
+    )
+    history[-1][1] = ""
+    for chunk in stream:
+        if chunk.choices[0].delta.content is not None:
+            history[-1][1] += chunk.choices[0].delta.content
+            yield history
+        
---
+++ utils/util.py
@@ -0,0 +1,38 @@
+import time
+from openai import OpenAI
+
+
+def register_openai_api_key(api_key):
+    if api_key:
+        return OpenAI(api_key=api_key)
+    else:
+        return None
+
+
+def get_conversation(system_prompt, history, message):
+    conversation = []
+
+    if system_prompt != "":
+        conversation.append({"role": "system", "content": system_prompt })
+
+    for human, assistant in history:
+        conversation.append({"role": "user", "content": human })
+        conversation.append({"role": "assistant", "content":assistant})
+
+    conversation.append({"role": "user", "content": message})
+    return conversation
+
+
+def save_button_event(history, title, dataframe):
+    if title == "":
+        return dataframe, ""
+
+    id = time.time()
+        
+    dataframe.loc[len(dataframe)] = [time.time(), title]
+    return dataframe, "", [["what", "the"]]
+
+
+def create_chat_id():
+    return time.time()
+
